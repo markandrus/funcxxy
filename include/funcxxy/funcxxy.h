19b2000a1caaa620b3fddbe673685d6f27cd7ed6
Mark Andrus Roberts <markandrusroberts@gmail.com>
2018-07-07 13:03:50 -0700

    Import code from node-webrtc

--- include/funcxxy/funcxxy.h
+++ include/funcxxy/funcxxy.h
@@ -1,5 +1,12 @@
-#pragma once
+#ifndef FUNCXXY_H_
+#define FUNCXXY_H_
+
+#include <funcxxy/curry.h>
+#include <funcxxy/maybe.h>
+#include <funcxxy/operators.h>
 
 namespace funcxxy {
   // :-)
 }  // namespace funcxxy
+
+#endif  // FUNCXXY_H_
--- test/src/main.cc
+++ test/src/main.cc
@@ -1,8 +1,41 @@
 #include <funcxxy/funcxxy.h>
 #include <gtest/gtest.h>
 
-TEST(funcxxyTest, test) {
-  ASSERT_TRUE(true);
+using namespace funcxxy;
+
+static bool toggle(bool a) {
+  return !a;
+}
+
+TEST(funcxxyTest, Maybe) {
+  auto result = toggle % Just(false);
+  ASSERT_TRUE(result.FromMaybe(false));
+}
+
+static int add2(int a, int b) {
+  return a + b;
+}
+
+TEST(funcxxyTest, Maybe2) {
+  auto result = curry(add2) % Just(1) * Just(2);
+  ASSERT_EQ(result.FromMaybe(0), 3);
+}
+
+TEST(funcxxyTest, Maybe3) {
+  auto result = Nothing<bool>() | Just(true);
+  ASSERT_TRUE(result.FromMaybe(false));
+}
+
+static int add3(int a, int b, int c) {
+  return a + b + c;
+}
+
+TEST(funcxxyTest, Maybe4) {
+  auto result = curry(add3)
+    % Just(1)
+    * Just(2)
+    * Just(3);
+  ASSERT_EQ(result.FromMaybe(0), 6);
 }
 
 int main(int argc, char **argv) {
